3042b547abd4807592f01aa93836dcc4f8ef6f3a
anhhdph40750-tech <anhhdph40750@fpt.edu.vn>
2026-03-09 16:05:09 +0700

    refactor: change account ducanh git and up again

--- src/main/java/com/certainshop/entity/Voucher.java
+++ src/main/java/com/certainshop/entity/Voucher.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Table(name = "Voucher")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class Voucher {
-// upgain check active
+// test
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Id")
